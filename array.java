3c7d33eaed41dcea5aa6ea3ebf7071acb54c6ea0
Geetansh Gupta <67698473+geetansh73@users.noreply.github.com>
2021-07-26 13:57:28 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,17 @@
+class array
+{
+public static void main(String geetu[])
+{
+int arr[]=new int[5];
+arr[0]=10;
+arr[1]=20;
+arr[2]=30;
+arr[3]=40;
+arr[4]=50;
+int i;
+for(i=0;i<arr.length;i++)
+{
+System.out.println(arr[i]);
+}
+}
+}
